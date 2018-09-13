ad94adcca63f645d6d7f5f3cb5ed1f245b02e46b
Jack Cooney <19coonej@elmbrookstudents.org>
2018-09-13 13:47:35 -0500

    Mailing Label

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jack Cooney \n1285 Weston Hills Ct \nBrookfield, WI 53045");
+	}
+
+}
