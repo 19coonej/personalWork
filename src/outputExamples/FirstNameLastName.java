fe41b4cb0d70d6eb5e1afe748619f88a81c800fc
Jack Cooney <19coonej@elmbrookstudents.org>
2018-09-13 13:09:58 -0500

    First Name Last Name

---
+++ src/outputExamples/FirstNameLastName.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FirstNameLastName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Jack");
+System.out.println("Cooney");
+
+	}
+
+}
