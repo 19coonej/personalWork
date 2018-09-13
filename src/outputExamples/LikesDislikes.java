784fb128938e11eb5cc64c5f1cb4e55d5c7dcbb4
Jack Cooney <19coonej@elmbrookstudents.org>
2018-09-13 13:44:38 -0500

    Likes and Dislikes

---
+++ src/outputExamples/LikesDislikes.java
@@ -0,0 +1,15 @@
+package outputExamples;
+
+public class LikesDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Likes \t \t Dislikes");
+		System.out.println("Pizza \t \t Olives");
+		System.out.println("Monkeys \t Dogs");
+		System.out.println("Gym \t \t Math");
+		System.out.println("NYC \t \t Jermoe");
+		System.out.println("Sour Patch \t Almond Joy");
+	}
+
+}
