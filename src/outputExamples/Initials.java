fc92350768858978a2507ffe99a7577a85cf5364
Jack Cooney <19coonej@elmbrookstudents.org>
2018-09-13 13:56:41 -0500

    Initials

---
+++ src/outputExamples/Initials.java
@@ -0,0 +1,16 @@
+package outputExamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JJJJJ   SSSSS     CCCCC");
+		System.out.println("  J     S         C");
+		System.out.println("  J     S         C");
+		System.out.println("  J     SSSSS     C");
+		System.out.println("  J         S     C");
+		System.out.println("  J         S     C");
+		System.out.println("JJJ     SSSSS     CCCCC");
+	}
+
+}
