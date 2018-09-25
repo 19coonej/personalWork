22763f82753b2408a63a7e861efa9421fac43642
Jack Cooney <19coonej@elmbrookstudents.org>
2018-09-25 15:02:48 -0500

    Phone Apart

---
+++ src/mathOperators/PhoneApart.java
@@ -0,0 +1,18 @@
+package mathOperators;
+import java.util.Scanner;
+public class PhoneApart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = -999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your phone number?");
+		number = input.nextInt();
+		System.out.println();
+		
+		
+		
+		
+	}
+
+}
