ac3bf4c083f1a57983c96668da877b616e251fe0
Jack Cooney <19coonej@elmbrookstudents.org>
2018-10-04 14:02:59 -0500

    Numbers OOP Work

---
+++ src/OOP/Numbers.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public class Numbers {
+public Numbers() {	
+}
+public void sayNumber(double num) {
+	System.out.println("Your number is: " + num);
+}
+public void sayNumberTwo(double num) {
+	System.out.println("Your number is: " + (num + 2));
+}
+public void sayNum(double num, double num2) {
+	System.out.println("Your number is: " + (num + num2));
+}
+}
---
+++ src/OOP/TestClassNumbers.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class TestClassNumbers {
+
+	public static void main(String[] args) {
+ Numbers man = new Numbers();
+ man.sayNumber(7.5);
+man.sayNumberTwo(36.8);
+man.sayNum(5.89, 84.6);
+	}
+
+}
