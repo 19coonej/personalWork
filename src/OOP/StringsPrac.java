514424dff1d8867b83a394c425d13c92060914e7
Jack Cooney <19coonej@elmbrookstudents.org>
2018-10-04 14:02:48 -0500

    Strings Practice

---
+++ src/OOP/StringsPrac.java
@@ -0,0 +1,14 @@
+package OOP;
+
+public class StringsPrac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	double num1 = 7.1;
+	double num1C = Math.pow(num1, 3);
+	System.out.println(num1C);	
+	num1 = Math.floor(num1);
+	System.out.println(	(num1));
+	}
+
+}
