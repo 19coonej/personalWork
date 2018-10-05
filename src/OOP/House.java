bb9804178186fc71e01d1c29900e639168ba99f2
Jack Cooney <19coonej@elmbrookstudents.org>
2018-10-05 15:04:36 -0500

    House Start

--- src/OOP/House.java
+++ src/OOP/House.java
@@ -1,5 +1,18 @@
 package OOP;
-
+import java.util.Scanner;
 public class House {
+	public House() {
+		
+	}
+public void paintWall() {
+	Scanner input = new Scanner(System.in);
+	System.out.println("What is the length of the wall?");
+	int length = input.nextInt();
+	System.out.println("What is the height of the wall?");
+	int height = input.nextInt();
+	int area = length * height;
+	
+}
+	
 
 }
